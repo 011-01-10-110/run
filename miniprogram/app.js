d66cdea82356eb838b1148a2f9320a15d8a72d91
wang <995854875@qq.com>
2020-12-08 20:50:28 +0800

    change

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -10,11 +10,36 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
+        env: 'wang-9f42b4',
         traceUser: true,
       })
     }
+    // 用户登录
 
-    this.globalData = {}
+    wx.getSetting({
+      success: (result) => {
+        // console.log(result);
+        if (result.authSetting['scope.userInfo']) {
+          if (!this.globalData.userInfo) {
+            // 获取用户信息
+            wx.getUserInfo({
+              success: (res) => {
+                // console.log(res);
+                this.globalData.userInfo = res.userInfo
+                if (this.callbackUser) {
+                  this.callbackUser(res)
+                }
+              },
+              fail: () => {}
+            });
+          }
+        }
+      },
+      fail: () => {}
+    });
+
+    this.globalData = {
+      userInfo: null
+    }
   }
 })
--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -7,110 +7,47 @@ Page({
    */
   data: {
     avatarUrl: '../../images/user-unlogin.png',
-    userInfo: {},
-    list:[
-      {icon:"../../images/my.png",text:"我的信息"},
-      
-    ]
+    userInfo: null,
+    list: [{
+      icon: "../../images/my.png",
+      text: "我的信息"
+    }, ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  onGetUserInfo: function(e) {
-  // 获取用户信息
-  wx.getSetting({
-    success: res => {
-      if (res.authSetting['scope.userInfo']) {
-        // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-        wx.getUserInfo({
-          success: res => {
-            this.setData({
-              avatarUrl: res.userInfo.avatarUrl,
-              userInfo: res.userInfo
-            })
-          }
+    if(app.globalData.userInfo){
+      this.setData({
+        userInfo: app.globalData.userInfo
+      })
+    }else{
+      app.callbackUser = (res)=>{
+        this.setData({
+          userInfo: res.userInfo
         })
       }
     }
-  })
-////////
-  if (!this.data.logged && e.detail.userInfo) {
-    this.setData({
-      logged: true,
-      avatarUrl: e.detail.userInfo.avatarUrl,
-      userInfo: e.detail.userInfo
+  },
+
+  /**
+   * 获取用户信息
+   */
+  onGetUserInfo: function (e) {
+    // 获取用户信息
+    wx.getUserInfo({
+      success: res => {
+        this.setData({
+          userInfo: res.userInfo
+        })
+      }
     })
-    console.log(this.data.userInfo)
-  }
-  // 调用云函数
-  wx.cloud.callFunction({
-    name: 'login',
-    data: {},
-    success: res => {
-      console.log('[云函数] [login] user openid: ', res.result.openid)
-      app.globalData.openid = res.result.openid
-    },
-    fail: err => {
-      console.error('[云函数] [login] 调用失败', err)
-    }
-  })
-  },
-
-//我的信息
-  mymessage:function(e){
-    
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  //我的信息
+  mymessage: function (e) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
